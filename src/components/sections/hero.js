7377fb7ff434bfd0da70b1d58f5aae06c2865bf6
FishingInBot <fishinginbot@gmail.com>
2025-05-05 12:33:31 -0500

    Began changing strings, still a lot left to do.

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,14 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Brandon Murry.</h2>;
+  const three = <h3 className="big-heading">I build things that solve problems.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m an application engineer that works to create microservices and APIs for{' '}
+        <a href="https://www.abbvie.com" target="_blank" rel="noreferrer">
+          AbbVie
         </a>
         .
       </p>
